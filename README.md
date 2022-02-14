87039a3c4c2c6bd505af9630c41ad63807c9143d
Kieron Day <kieron.day@email.com>
2022-02-14 13:47:49 +0000

    Ticket 3 - Complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Northcoders News API - Portfolio Project
+
+Create two .env files to locally connect to the two databases: `.env.test` and `.env.development`. Into each, add `PGDATABASE=<database_name_here>`, with the correct database name for that environment.
---
+++ __tests__/app.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const app = require("../app.js");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+
+beforeEach(() => {
+	return seed(data);
+});
+
+describe("app", () => {
+	describe("GET Request - /api/topics", () => {
+		test("Status: 200", () => {
+			return request(app).get("/api/topics").expect(200);
+		});
+		test("Status: 200, responds with an array", () => {
+			return request(app)
+				.get("/api/topics")
+				.expect(200)
+				.then(({ body: { topics } }) => {
+					expect.any(Array.isArray(topics) === true);
+				});
+		});
+		test("Status: 200, responds with array with 3 elements", () => {
+			return request(app)
+				.get("/api/topics")
+				.expect(200)
+				.then(({ body: { topics } }) => {
+					expect(topics).toHaveLength(3);
+				});
+		});
+		test("Status: 200, responds with array of topic objects", () => {
+			return request(app)
+				.get("/api/topics")
+				.expect(200)
+				.then(({ body: { topics } }) => {
+					topics.forEach((topic) => {
+						expect(topic).toEqual(
+							expect.objectContaining({
+								slug: expect.any(String),
+								description: expect.any(String),
+							})
+						);
+					});
+				});
+		});
+	});
+});
